e7aea10e89582e36ed9c744e217f05c410ecde0d
odajunpei <junpeioda.creat@gmail.com>
2022-06-07 22:46:51 +0000

    aa

--- app/controllers/rainbot_controller.rb
+++ app/controllers/rainbot_controller.rb
@@ -36,10 +36,10 @@ class RainbotController < ApplicationController
             per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
-                "明日の天気だよね。\n明日は雨が降るぜぇ\n降水確率はこんな感じだぜぇ\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n明日の朝、最新の天気予報で雨が降りそうだったら教えるぜぇ！"
+                "明日の天気は、\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n明日の朝、最新の天気予報で雨が降りそうか送るよ！"
             else
               push =
-                "明日の天気？\n教えないよ\n世間はそんなに甘くないからね！"
+                "明日の天気？\n教えないよ\n世間はそんなに甘くないからね！(晴れ)"
             end
           when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]'].text
@@ -47,10 +47,10 @@ class RainbotController < ApplicationController
             per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]'].text
             if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
               push =
-                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+                "明後日は雨が降りそう…\n当日の朝に雨が降りそうだったらラインするよ！"
             else
               push =
-                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+                "明後日は雨は降らない予定！\nまた当日の朝の最新の天気予報で雨が降りそうだったラインするよ！"
             end
           when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
             push =
@@ -58,6 +58,9 @@ class RainbotController < ApplicationController
           when /.*(こんにちは|こんばんは|初めまして|はじめまして|おはよう).*/
             push =
               "こんにちは。\n声をかけてくれてありがとう\n今日があなたにとっていい日になりますように(^^)"
+          when /.*(おみくじ).*/
+            push =
+              "今日は後輩にジュースを奢ると運気アップ間違いなし！"
           else
             per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
@@ -68,15 +71,12 @@ class RainbotController < ApplicationController
                  "雨に負けずファイト！！",
                  "雨だけどあなたの明るさでみんなを元気にしてあげて(^^)"].sample
               push =
-                "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
+                "今日は雨が降りそうだから傘があった方が安心！\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
             else
               word =
-                ["天気もいいから一駅歩いてみるのはどう？(^^)",
-                 "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
-                 "素晴らしい一日になりますように(^^)",
-                 "雨が降っちゃったらごめんね(><)"].sample
+                ["素晴らしい一日になりますように！"].sample
               push =
-                "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
+                "今日は雨は降らなさそうだよ。\n#{word}"
             end
           end
           # テキスト以外（画像等）のメッセージが送られた場合
